5a19440cff2910d3ff39a5680c450732ad5ca03a
matt <mattcollie@live.com>
2017-07-22 22:51:18 +1200

    added xptables for skillType object

---
+++ ClanScape/ClanScape.Data.Objects/Tables/Skill.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClanScape.Data.Objects.Tables
+{
+    [Table("Skills")]
+    public class Skill
+    {
+        [Key]
+        public long Id { get; set; }
+
+        public int CurrentXP { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public double PercentageToLevel
+        {
+            get
+            {
+                return SkillType.CalculateCurrentLevel(CurrentXP);
+            }
+            private set { }
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public int CurrentLevel
+        {
+            get
+            {
+                return SkillType.CalculateCurrentLevel(CurrentXP);
+            }
+            private set { }
+        }
+        
+        public int SkillTypeId { get; set; }
+
+        [ForeignKey("SkillTypeId")]
+        public virtual SkillType SkillType { get; set; }
+    }
+}
--- ClanScape/ClanScape.Data.Objects/Tables/SkillType.cs
+++ ClanScape/ClanScape.Data.Objects/Tables/SkillType.cs
@@ -1,14 +1,47 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ClanScape.Data.Objects.Tables
 {
-    [Table("SkillType")]
+    [Table("SkillTypes")]
     public partial class SkillType
     {
         [Key]
         public int Id { get; set; }
         
         public string Type { get; set; }
+
+        public ICollection<Skill> Skills { get; set; }
+
+        public int CalculateCurrentLevel(int currentXP)
+        {
+            int level = 0;
+
+            switch(Type)
+            {
+                case "Normal":
+                    return Common.Helpers.XPTable.CalculateCurrentNormalLevel(currentXP);
+                case "Elite":
+                    return Common.Helpers.XPTable.CalculateCurrentEliteLevel(currentXP);
+            }
+
+            return level;
+        }
+
+        public double CalculatePercentageToLevel(int currentXP)
+        {
+            double percentage = 1;
+
+            switch (Type)
+            {
+                case "Normal":
+                    return Common.Helpers.XPTable.CalculatePercentageToNormalLevel(currentXP);
+                case "Elite":
+                    return Common.Helpers.XPTable.CalculatePercentageToEliteLevel(currentXP);
+            }
+
+            return percentage;
+        }
     }
 }
